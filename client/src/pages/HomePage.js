16db7000d87f1251ce580c7c1b379d52d4c0753b
rishabhguptajs <rishabhgupta4523@gmail.com>
2023-12-25 23:22:34 +0530

    ....

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layouts/Layout'
+
+const HomePage = () => {
+  return (
+    <Layout >
+        <h1> Homepage </h1>
+    </Layout>
+  )
+}
+
+export default HomePage
