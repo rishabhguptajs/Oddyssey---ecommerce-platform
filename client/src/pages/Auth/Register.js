d2aaa4a12c1e758bc21af5f4dea24bf223d102b1
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-01-03 23:12:59 +0530

    coded basic register page

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -5,106 +5,61 @@ import { Link } from "react-router-dom";
 const Register = () => {
   return (
     <Layout>
-      <main className="flex flex-col items-center justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div className="text-center">
-            <h2 className="font_styling mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Create your account
-            </h2>
-          </div>
-          <div
-            className="rounded-lg border bg-card text-card-foreground shadow-sm"
-            data-v0-t="card"
-          >
-            <div className="flex flex-col space-y-1.5 px-6 pt-8 text-center text-xl font-bold font_styling">
-              Registration Form
-            </div>
-            <div className="p-6 mt-1 ">
-              <form className="space-y-6" action="#" method="POST">
-                <div>
-                  <label
-                    className="font_styling text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    htmlFor="name"
-                  >
-                    Name
-                  </label>
-                  <input
-                    className="h-10 bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    id="name"
-                    required
-                    placeholder="Rishabh"
-                    type="text"
-                    name="name"
-                  />
-                </div>
-                <div>
-                  <label
-                    className="font_styling text-sm font-medium"
-                    htmlFor="email"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="h-10 bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    id="email"
-                    autoComplete="email"
-                    placeholder="rishabh@example.com"
-                    type="email"
-                    name="email"
-                  />
-                </div>
-                <div>
-                  <label
-                    className="font_styling text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    htmlFor="location"
-                  >
-                    Location
-                  </label>
-                  <input
-                    className="h-10 bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    id="location"
-                    autoComplete="street-address"
-                    required
-                    placeholder="Bangalore"
-                    type="text"
-                    name="location"
-                  />
-                </div>
-                <div>
-                  <label
-                    className="font_styling text-sm font-medium"
-                    htmlFor="phone"
-                  >
-                    Phone Number
-                  </label>
-                  <input
-                    className="h-10 rounded-sm bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#e13453] focus:border-[#4a3f41] focus:z-10 sm:text-sm"
-                    id="phone"
-                    autoComplete="tel"
-                    placeholder="+91 9876543210"
-                    type="tel"
-                    name="phone"
-                  />
-                </div>
-                <div>
-                  <button
-                    className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
-                    type="submit"
-                  >
-                    Register
-                  </button>
-                </div>
-              </form>
+      <div className="align-middle flex items-center w-full p-5 h-full">
+        <form
+          action=""
+          className="align-middle w-fit p-5 flex flex-col items-center border-solid border-1 border-gray-400 mx-auto rounded-md shadow-[#e13453] shadow-medium"
+        >
+          <h1 className="py-3">Register Form</h1>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName" className="form-label w-3">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control w-[20vw]"
+              id="exampleInputName"
+              aria-describedby="nameHelp"
+              name="name"
+            />
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control w-[20vw]"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              name="email"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
             </div>
           </div>
-          <div className="text-center text-sm">
-            Already have an account? &nbsp;
-            <Link className="underline" to="/login">
+          <div className="mb-3">
+            <label
+              htmlFor="exampleInputPassword1"
+              className="form-label w-[20vw]"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+            />
+          </div>
+          <div>
+            <Link to="/login" className="btn btn-primary bg-blue-600">
               Login
             </Link>
+            <button type="submit" className="btn btn-primary bg-blue-600">
+              Submit
+            </button>
           </div>
-        </div>
-      </main>
+        </form>
+      </div>
     </Layout>
   );
 };
