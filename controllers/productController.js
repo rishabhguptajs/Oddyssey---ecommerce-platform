561f5b3728b9cfb55085371ce6263d1edf1c4b10
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-01-21 00:46:59 +0530

    CRUD - Create, Read, Update, Delete of Products API done

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,85 +2,147 @@ import productModel from "../models/productModel.js";
 import fs from "fs";
 import slugify from "slugify";
 
-export const createProductController = async( req, res ) => {
-    try {
-        const { name, slug, price, category, description, quantity, shipping } = req.fields;
-        const { photo } = req.files;
+export const createProductController = async (req, res) => {
+  try {
+    const { name, slug, price, category, description, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
 
-        if( !name  || !price || !category || !description || !quantity || !photo ) {
-            return res.status(400).json({
-                success: false,
-                error: "All fields are required."
-            })
-        }
-        const product = new productModel({...req.fields, slug: slugify(name)});
-        if(photo){
-            product.photo.data = fs.readFileSync(photo.path);
-            product.photo.contentType = photo.type;
-        }
-        await product.save();
-        res.status(201).json({
-            success: true,
-            message: "Product created successfully.",
-            product
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            error: "Internal server error."
-        })
+    if (!name || !price || !category || !description || !quantity || !photo) {
+      return res.status(400).json({
+        success: false,
+        error: "All fields are required.",
+      });
     }
-}
-
-export const getProductsController = async(req, res) => {
-    try {
-        const products = await productModel.find({}).populate("category").select('-photo').limit(10).sort({createdAt: -1});
-        res.status(200).json({
-            success: true,
-            countTotal: products.length,
-            message: "Products fetched successfully.",
-            products
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            error: "Error while getting products."
-        })
+    const product = new productModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
     }
-}
+    await product.save();
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully.",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Internal server error.",
+    });
+  }
+};
 
+export const getProductsController = async (req, res) => {
+  try {
+    const products = await productModel
+      .find({})
+      .populate("category")
+      .select("-photo")
+      .limit(10)
+      .sort({ createdAt: -1 });
+    res.status(200).json({
+      success: true,
+      countTotal: products.length,
+      message: "Products fetched successfully.",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Error while getting products.",
+    });
+  }
+};
 
-export const getSingleProductController = async(req, res) => {
-    try {
-        const product = await productModel.findOne({slug: req.params.slug}).select('-photo').populate("category");
-        res.status(200).json({
-            success: true,
-            message: "Product fetched successfully.",
-            product
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            error: "Error while getting product."
-        })
+export const getSingleProductController = async (req, res) => {
+  try {
+    const product = await productModel
+      .findOne({ slug: req.params.slug })
+      .select("-photo")
+      .populate("category");
+    res.status(200).json({
+      success: true,
+      message: "Product fetched successfully.",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Error while getting product.",
+    });
+  }
+};
+
+export const productPhotoController = async (req, res) => {
+  try {
+    const product = await productModel
+      .findById({ slug: req.params.pid })
+      .select("photo");
+    if (product.photo.data) {
+      res.set("Content-Type", product.photo.contentType);
+      return res.send(product.photo.data);
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Error while getting photo.",
+    });
+  }
+};
 
-export const productPhotoController = async(req, res) => {
-    try {
-        const product = await productModel.findById({slug: req.params.pid}).select('photo');
-        if(product.photo.data){
-            res.set("Content-Type", product.photo.contentType);
-            return res.send(product.photo.data);
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            error: "Error while getting photo."
-        })
+export const deleteProductController = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.params.pid).select("-photo");
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Error while deleting product.",
+    });
+  }
+};
+
+export const updateProductController = async (req, res) => {
+  try {
+    const { name, slug, price, category, description, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+
+    if (!name || !price || !category || !description || !quantity || !photo) {
+      return res.status(400).json({
+        success: false,
+        error: "All fields are required.",
+      });
     }
-}
+    const product = await productModel.findByIdAndUpdate(
+      req.params.pid,
+      { ...req.fields, slug: slugify(name) },
+      { new: true }
+    );
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
+    }
+    await product.save();
+    res.status(201).json({
+      success: true,
+      message: "Product updated successfully.",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "Error while updating product.",
+    });
+  }
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,18 +1,40 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { createProductController, getProductsController, getSingleProductController, productPhotoController } from "../controllers/productController.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductsController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productController.js";
 import formidale from "express-formidable";
 
 const router = express.Router();
 
 // routes here
 
-router.post('/create-product', requireSignIn, isAdmin, formidale(), createProductController);
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidale(),
+  createProductController
+);
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidale(),
+  updateProductController
+);
 
-router.get('/get-product', getProductsController);
+router.get("/get-product", getProductsController);
 
-router.get('/get-product/:slug', getSingleProductController);
+router.get("/get-product/:slug", getSingleProductController);
 
-router.get('/product-photo/:pid', productPhotoController);
+router.get("/product-photo/:pid", productPhotoController);
+
+router.delete("/product/:pid", deleteProductController);
 
 export default router;
