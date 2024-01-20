40b5b9ec1f69ec4ed4d42f1eed2bbe9a937f4272
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-01-21 01:11:52 +0530

    made some changes and api request to create category

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layouts/Layout";
 import AdminMenu from "../../components/Layouts/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { get } from "mongoose";
+
+const statusColorMap = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  // function for getting all categories
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data.success) {
+        setCategories(data.categories);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Error while getting categories.");
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
   return (
     <Layout title={" Dashboard - Create Category "}>
       <div className="flex flex-row">
@@ -10,7 +37,29 @@ const CreateCategory = () => {
           <AdminMenu />
         </div>
         <div className="flex flex-col justify-center m-5 hover:shadow-md transition-shadow w-fit h-inherit p-4 border-[#c5cae9] border-4">
-          <h1 className="font-bold font_styling p-3 h-fit w-full">Create Category</h1>
+          <h1 className="font-bold font_styling p-3 h-fit w-full">
+            Manage Categories
+          </h1>
+          <div>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {categories.map((category) => (
+                  <tr>
+                    <td key={category._id}>{category.name}</td>
+                    <td>
+                      <button className="btn btn-primary">Edit</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </Layout>
