166250b59a51ae75c923e9b22051324ffb4865f6
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-10-13 18:04:06 +0530

    refactor api call

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -17,7 +17,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const res = await axios.post("/api/v1/auth/register", {
+      const res = await axios.post(`${process.env.REACT_API}/api/v1/auth/register`, {
         name,
         email,
         password,
